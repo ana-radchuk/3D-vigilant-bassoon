4e58bfabfbce89c96ad502c94f6af283a22e6299
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2024-01-14 16:02:30 -0500

    added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,10 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      {/* <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
+
+      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto 
+          ${styles.paddingX} flex flex-row items-start gap-5`}>
+
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -16,16 +17,16 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Anastasia</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Dedicated and innovative developer <br className='sm:block hidden' />
+            for crafting efficient software solutions.   
           </p>
         </div>
-      </div> */}
+      </div>
 
-      {/* <ComputersCanvas />
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
@@ -43,7 +44,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div> */}
+      </div>
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Anastasiia &nbsp;
+            Anastasia &nbsp;
             <span className="sm:block hidden">
               | Java Full-stack Developer
             </span>
